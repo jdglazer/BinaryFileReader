2169514cc98ed5ba7b93c5f6bde5b2fdcbc48f39
jdglazer <atrail2014@gmail.com>
2016-04-12 18:18:13 -0400

    Add Updated Main Code File

--- BinaryFile.php
+++ BinaryFile.php
@@ -1,8 +1,11 @@
 <?php
-	
+/**
+ * A php class for reading binary files. The class has been tested 
+ * 
+ * @author Glazer, Joshua D.
+ */
 	class BinaryFile {
 		
-		//pool variables
 		private $bin_str;
 		
 		private $_fileptr;
@@ -13,12 +16,27 @@
 		
 		private $type;
 		
+		public static $SHORT = 's';
+		
+		public static $INT = 'i';
+		
+		public static $LONG = 'l';
+		
+		public static $FLOAT = 'f';
+		
+		public static $DOUBLE = 'd';
+		
 		private $valid_types = Array( "d" => 8, 
 										"f" => 4,
 										 "l" => 8,
 										 "i" => 4,
 										 "s" => 2 );
-		
+/**
+ * Constructor
+ * @param $file Takes either an address to a file or a file resource returned from fopen()
+ * @param $openas Tells fopen() the mode in which to open file. See php documentation for mode parameter in <a href="http://php.net/manual/en/function.fopen.php">fopen()</a> for valid values
+ * @throws Exception If invalid input type is passed to the first argument (not string or object)
+ */ 		
 		function __construct( $file, $openas ) {
 			
 			if( is_string( $file ) ) {
@@ -39,6 +57,9 @@
 			
 		}
 		
+/**
+ * Sets default values for endianness, namely big endian and double type
+ */
 		private function _const() {
 			
 			$this->endianness = "B";
@@ -47,6 +68,9 @@
 			
 		}
 		
+/**
+ * Determined machine endianness
+ */ 
 		private function setMachineEndianness() {
 			
 			$sampleByteStream =	"abcd";
@@ -63,7 +87,14 @@
 		}
 		
 
-//WORK ON THE READ FUNCTIONALITY
+/**
+ * Gets a specified type from a specified position in the file
+ * 
+ * @param $type A string indicating the type to return. Use the static properites of the class for ease ( BinaryFile->SHORT, BinaryFile->INT, BinaryFile->LONG, BinaryFile->FLOAT, BinaryFile->DOUBLE )
+ * @param $litEnd boolean set to true if value is to be read as little endian, false for big endian
+ * @param $offset The offset in bytes in the file to begin reading value from
+ * @return value being read or false if invalid $type argument is passed in
+ */
 		public function read( $type, $litEnd, $offset ) {
 			
 			if( $this->setType( $type ) ) {
@@ -78,24 +109,41 @@
 				
 					$this->bin_str = strrev( $this->bin_str );
 						
-				return unpack( $type,  $this->bin_str )[1];
+				$r = unpack( $type,  $this->bin_str );
+				
+				if( is_array($r) )
+				
+					return $r[1];
+				else 
+				
+					return false;
 			}
 			else
 				return false;
 			
 						
 		}
-		
+/**
+ * Sets the values to be read as little endian
+ */
 		public function setLittleEnd( ) {
 			
 			$this->endianness = "L";
 		}
-		
+
+/**
+ * Set the values to be rad as big endian
+ */		
 		public function setBigEnd( ) {
 			
 			$this->endianness = "B";
 		}
-		
+
+/**
+ * Sets the type to be read from the file
+ * @param $type String indicating the type to be read from the file ( BinaryFile->SHORT, BinaryFile->INT, BinaryFile->LONG, BinaryFile->FLOAT, BinaryFile->DOUBLE )
+ * @return returns true if the $type value passed in is valid identifier
+ */		
 		public function setType( $type ) {
 			
 			if( array_key_exists( $type, $this->valid_types ) ) {
